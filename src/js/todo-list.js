34704ded44154c920cf7cd041798179eb176fc6b
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-03-23 23:52:44 +0900

    feat: todo list의 체크박스를 클릭하여 complete 상태로 변경 기능 추가

--- src/js/todo-list.js
+++ src/js/todo-list.js
@@ -4,27 +4,31 @@ function enter(element) {
       return;
     }
 
-    const button_destroy = document.createElement('button');
-    button_destroy.setAttribute('class', 'destroy');
+    const li = document.createElement('li');
 
+    const button_destroy = document.createElement('button');
+
+    button_destroy.setAttribute('class', 'destroy');
     const label = document.createElement('label');
     label.setAttribute('class', 'label');
-    label.innerText = element.value;
 
+    label.innerText = element.value;
     const input_toggle = document.createElement('input');
     input_toggle.setAttribute('class', 'toggle');
     input_toggle.setAttribute('type', 'checkbox');
-
+    input_toggle.onclick = function checked(event) {
+      input_toggle.checked = true;
+      li.setAttribute('class', 'toggle completed');
+    }
     const input_edit = document.createElement('input');
     input_edit.setAttribute('class', 'edit');
-    input_edit.setAttribute('value', element.value);
 
+    input_edit.setAttribute('value', element.value);
     const div_view = document.createElement('div');
     div_view.appendChild(input_toggle);
     div_view.appendChild(label);
-    div_view.appendChild(button_destroy);
 
-    const li = document.createElement('li');
+    div_view.appendChild(button_destroy);
     li.appendChild(div_view);
     li.appendChild(input_edit);
 
@@ -32,8 +36,4 @@ function enter(element) {
 
     document.getElementById('new-todo-title').value = null;
   }
-}
-
-function checked(element) {
-
 }
