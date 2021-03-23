7c357bc66ae0e4804a20d0bc6ccfce23ecb6aafc
멍토 <daum7766@naver.com>
2021-03-24 02:01:59 +0900

    feat: 필요한 기능 구현

---
+++ src/js/function.js
@@ -0,0 +1,158 @@
+const TODO = document.getElementById("todo-list");
+const TODO_INPUT = document.getElementById("new-todo-title");
+const CONTENT_COUNT = document.getElementById("content-count");
+const ALL_VIEW = document.getElementById("all");
+const ACTIVE_VIEW = document.getElementById("active");
+const COMPLETED_VIEW = document.getElementById("completed");
+const ENTER = "Enter";
+const ESC = "Escape";
+
+
+function addItem(event) {
+    if (event.key === ENTER && TODO_INPUT.value.trim() !== "") {
+        const inputData = TODO_INPUT.value.trim();
+        TODO_INPUT.value = "";
+
+        createTempliate(inputData)
+
+        TODO_INPUT.value = "";
+        countChange(getUrlType());
+    }
+}
+
+function createTempliate(inputData) {
+    const newList = createList();
+    const newDiv = createDiv();
+    const newCheckbox = createCheckbox();
+    const newLabel = createLabel(inputData);
+    const newButton = createButton();
+    const hideInput = createHideInput(inputData);
+    
+    newDiv.appendChild(newCheckbox);
+    newDiv.appendChild(newLabel);
+    newDiv.appendChild(newButton);
+    newList.appendChild(newDiv);
+    newList.appendChild(hideInput);
+    TODO.appendChild(newList);
+}
+
+function createList() {
+    const newList = document.createElement("li");
+    newList.className = "false";
+    newList.ondblclick = (event) => {
+        const parentList = event.target.parentNode.parentNode
+        parentList.className += " editing";
+        parentList.children[1].display = "block";
+    }
+    return newList;
+}
+
+function createDiv() {
+    const newDiv = document.createElement("div");
+    newDiv.className = "view";
+    return newDiv;
+}
+
+function createCheckbox() {
+    const newCheckbox = document.createElement("input");
+    newCheckbox.type = "checkbox";
+    newCheckbox.className = "toggle";
+    newCheckbox.onclick = (event) => {
+        const parentList = event.target.parentNode.parentNode;
+        if (parentList.className === "false") {
+            parentList.className = "completed";
+        } else {
+            parentList.className = "false";
+        }
+        countChange(getUrlType());
+    }
+    return newCheckbox;
+}
+
+function createLabel(inputData) {
+    const newLabel = document.createElement("label");
+    newLabel.textContent = inputData;
+    newLabel.className = "label";
+    return newLabel;
+}
+
+function createButton() {
+    const newButton = document.createElement("button");
+    newButton.className = "destroy";
+    newButton.onclick = (event) => {
+        const data = event.target.previousSibling.textContent;
+        if (confirm(data + "을(를) 지우시겠습니까?") === false) {
+            return;
+        }
+        const parentList = event.target.parentNode.parentNode;
+        parentList.parentNode.removeChild(parentList);
+        countChange(getUrlType());
+    }
+    return newButton;
+}
+
+function createHideInput(inputData) {
+    const hideInput = document.createElement("input");
+    hideInput.className = "edit";
+    hideInput.value = inputData;
+    hideInput.display = "none";
+    hideInput.onkeydown = (event) => {
+        console.log(event.key);
+        if (event.key === ESC) {
+            event.target.display = "none";
+            event.target.parentNode.className = event.target.parentNode.className.split(" ")[0];
+            return;
+        } 
+        if (event.key === ENTER) {
+            event.target.previousSibling.children[1].textContent = event.target.value;
+            event.target.display = "none";
+            event.target.parentNode.className = event.target.parentNode.className.split(" ")[0];
+        }
+    }
+    return hideInput;
+}
+
+function countChange(urlType) {
+    if (urlType === "#") {
+        countAll();
+    }
+    else if (urlType === "#active") {
+        countClass("false");
+    }
+    else if (urlType === "#completed") {
+        countClass("completed")
+    }
+}
+
+function countAll() {
+    CONTENT_COUNT.textContent = TODO.children.length;
+}
+
+function countClass(className) {
+    let count = 0;
+    for (const todoItem of TODO.children) {
+        if (todoItem.className === className) {
+            count++;
+        }
+    }
+    CONTENT_COUNT.textContent = count;
+}
+
+function changeSelect(event) {
+    ALL_VIEW.className = "";
+    ACTIVE_VIEW.className = "";
+    COMPLETED_VIEW.className = "";
+
+    event.target.className = "selected";
+    if (event.target === ALL_VIEW) {
+        countAll();
+    } else if (event.target === ACTIVE_VIEW) {
+        countClass("false");
+    } else if (event.target === COMPLETED_VIEW) {
+        countClass("completed");
+    }
+}
+
+function getUrlType() {
+    return window.location.href.split("/")[3];
+}
