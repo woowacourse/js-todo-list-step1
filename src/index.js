29029b4e480722a7bc60ab98ec60339feaf83449
Wannte <T.wannte@gmail.com>
2021-03-23 21:29:44 +0900

    feat(index): complete 상태로 변경

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 let inputText = document.getElementById('new-todo-title');
+const todoList = document.getElementById('todo-list');
 
 inputText.addEventListener("keydown", addTodo);
 
@@ -8,20 +9,31 @@ function addNewItem(list, itemText){
 
 function createHtml(itemText) {
     let kanban = document.createElement('li');
-    kanban.innerHTML = '<div class="view">' + 
-    '<input class="toggle" type="checkbox"/>' + 
-    '<label class="label">' +
-    itemText +
-    '</label>' +
-    '<button class="destroy"></button>' +
-    '</div>';
+    kanban.innerHTML =
+        `<div class="view">
+            <input class="toggle" type="checkbox"/> 
+                <label class="label">${itemText}</label>
+            <button class="destroy"></button>
+        </div>
+        <input class="edit" value="새로운 타이틀" />`;
+    let toggleElem = kanban.querySelector('.toggle');
+    toggleElem.addEventListener('change', event => {
+        if (event.target.checked) {
+            kanban.classList.add('completed');
+            toggleElem.setAttribute('checked', '');
+
+        } else {
+            kanban.classList.remove('completed');
+            toggleElem.removeAttribute('checked');
+        }
+    });
     return kanban
 }
 
 function addTodo(e) {
     if (e.key === "Enter") {
         let itemText = inputText.value;
-        addNewItem(document.getElementById('todo-list'), itemText);
+        addNewItem(todoList, itemText);
         inputText.value = '';
     }
 }
