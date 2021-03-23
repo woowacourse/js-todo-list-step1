cc9f7138bc10cc18b7c69e7bee85e60c3e2cbcb3
hoony <younghoon.kwon91@gmail.com>
2021-03-23 17:07:38 +0900

    feat: 체크박스 클릭하였을때 completed class 추가, input태그에 checked속성 추가 구현

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,15 +1,36 @@
-$(document).on('keypress', function (e) {
-    if (e.which == 13) {
-        const todoItem = $("#new-todo-title").val();
-        $("#todo-list").append
-        ("  <li>\n" +
-            "    <div class=\"view\">\n" +
-            "      <input class=\"toggle\" type=\"checkbox\"/>\n" +
-            "      <label class=\"label\">" + todoItem + "</label>\n" +
-            "      <button class=\"destroy\"></button>\n" +
-            "    </div>\n" +
-            "    <input class=\"edit\" value=\"새로운 타이틀\" />\n" +
-            "  </li>");
+document.addEventListener('keypress', function (e) {
+    if (e.key == 'Enter') {
+        const todoItem = document.getElementById("new-todo-title").value;
+        addTodo(todoItem);
+    }})
+
+
+
+function addTodo(input) {
+    const todoTag = document.createElement('li');
+    todoTag.classList.add(false);
+    todoTag.innerHTML =
+        ` <div class="view">
+                <input class="toggle" type="checkbox" onclick="clicked(this);"/>
+                <label class="label">${input}</label>
+                <button class="destroy"></button>
+               </div>
+               <input class="edit" value="${input}"/>`;
+    document.getElementById("todo-list").appendChild(todoTag);
+}
+
+function clicked(self){
+    classList = self.parentNode.parentElement.classList;
+    if(classList.contains("false")){
+        self.setAttribute("checked", "");
+        classList.replace("false", "completed");
+        return;
     }
-});
+    if(classList.contains("completed")){
+        self.removeAttribute("checked")
+        classList.replace("completed", "false");
+        return;
+    }
+}
+
 
