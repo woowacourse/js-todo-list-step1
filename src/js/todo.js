d0f17035baa50f4cddc6e11fbad4f4c49513560d
hoony <younghoon.kwon91@gmail.com>
2021-03-23 17:56:31 +0900

    feat: 수정 완료후 esc 버튼 작동

--- src/js/todo.js
+++ src/js/todo.js
@@ -2,25 +2,33 @@ document.addEventListener('keypress', function (e) {
     if (e.key == 'Enter') {
         const todoItem = document.getElementById("new-todo-title").value;
         addTodo(todoItem);
-    }})
+    }
+})
 
 
 
 function addTodo(input) {
     const todoTag = document.createElement('li');
     todoTag.classList.add(false);
-    todoTag.ondblclick = function () {
-        todoTag.classList.add("editing");
-    };
     todoTag.innerHTML =
         ` <div class="view">
                 <input class="toggle" type="checkbox" onclick="clicked(this);"/>
-                <label class="label">${input}</label>
+                <label class="label" ondblclick="doubleClick(this)">${input}</label>
                 <button class="destroy" onclick="remove(this)"></button>
                </div>
                <input class="edit" value="${input}"/>`;
     document.getElementById("todo-list").appendChild(todoTag);
 
+}
+
+function doubleClick(self) {
+    classList = self.parentNode.parentElement.classList;
+    classList.add("editing");
+    document.addEventListener('keydown', function (e) {
+        if(e.key == 'Escape'){
+            classList.remove("editing")
+        }
+    })
 }
 
 function clicked(self){
