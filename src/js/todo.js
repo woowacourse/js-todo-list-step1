de9d52ea6772d0fedd43d8c0d2add12dd3b88a82
hoony <younghoon.kwon91@gmail.com>
2021-03-23 14:41:20 +0900

    feat: 엔터 눌렀을 때 todo-item 추가 기능 구현

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,2 +1,15 @@
-console.log("connected");
+$(document).on('keypress', function (e) {
+    if (e.which == 13) {
+        const todoItem = $("#new-todo-title").val();
+        $("#todo-list").append
+        ("  <li>\n" +
+            "    <div class=\"view\">\n" +
+            "      <input class=\"toggle\" type=\"checkbox\"/>\n" +
+            "      <label class=\"label\">" + todoItem + "</label>\n" +
+            "      <button class=\"destroy\"></button>\n" +
+            "    </div>\n" +
+            "    <input class=\"edit\" value=\"새로운 타이틀\" />\n" +
+            "  </li>");
+    }
+});
 
