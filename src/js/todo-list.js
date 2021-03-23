af6b141cee33d112490b68bc73acf16fd6b1969a
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-03-24 02:07:02 +0900

    feat: 체크 되어있는 todo list의 체크박스를 클릭하여 complete 되지 않은 상태로 변경

--- src/js/todo-list.js
+++ src/js/todo-list.js
@@ -1,6 +1,6 @@
-function enter(element) {
+function enter(input_new_todo_list) {
   if (event.keyCode === 13) {
-    if (element.value === '') {
+    if (input_new_todo_list.value === '') {
       return;
     }
 
@@ -14,29 +14,50 @@ function enter(element) {
 
     const label = document.createElement('label');
     label.setAttribute('class', 'label');
+    label.innerText = input_new_todo_list.value;
 
-    label.innerText = element.value;
     const input_toggle = document.createElement('input');
     input_toggle.setAttribute('class', 'toggle');
     input_toggle.setAttribute('type', 'checkbox');
     input_toggle.onclick = function checked() {
-      input_toggle.checked = true;
-      li.setAttribute('class', 'toggle completed');
+      if (input_toggle.checked) {
+        li.setAttribute('class', 'completed');
+        return;
+      }
+      li.classList.remove('completed');
     }
+
     const input_edit = document.createElement('input');
     input_edit.setAttribute('class', 'edit');
+    input_edit.setAttribute('value', input_new_todo_list.value);
 
-    input_edit.setAttribute('value', element.value);
+
     const div_view = document.createElement('div');
+    div_view.setAttribute('class', 'view');
+
     div_view.appendChild(input_toggle);
     div_view.appendChild(label);
-
     div_view.appendChild(button_destroy);
+
+    li.ondblclick = function edit() {
+      li.setAttribute('class', 'editing');
+    };
+
     li.appendChild(div_view);
     li.appendChild(input_edit);
 
     document.getElementById('todo-list').appendChild(li);
 
     document.getElementById('new-todo-title').value = null;
+  }
+}
+
+function editEnter(input_edited_todo_list, li) {
+  if (event.keyCode === 13) {
+    alert('수정 엔터 키 입력');
+    return;
+  }
+  if (event.keyCode === 27) {
+    alert('수정 esc 키 입력');
   }
 }
