3070fdfa311510f754317c35df71f92d472860d9
DWL5 <ssop6403@gmail.com>
2021-03-24 23:58:58 +0900

    feat: todo list를 더블클릭했을 때 input 모드로 변경.

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ function addTodoItem(e) {
     }
     const todoList = document.getElementById('todo-list');
     const todoTitle = document.getElementById('new-todo-title').value;
-    console.log(todoTitle);
+
+    const div = document.createElement('div');
+    div.setAttribute('class', 'view');
 
     const li = document.createElement('li');
-    li.setAttribute('class', 'editing')
+    li.addEventListener('dblclick', changeInputMode)
 
     const input = document.createElement('input');
     input.setAttribute('type', 'checkbox');
@@ -25,16 +27,20 @@ function addTodoItem(e) {
     button.setAttribute('class', 'destroy');
     button.addEventListener('click', deleteTodoItem)
 
-    li.appendChild(input);
-    li.appendChild(title);
-    li.appendChild(button);
-
+    const editInput = document.createElement('input')
+    editInput.setAttribute('class', 'edit');
+    editInput.setAttribute('value', todoTitle);
+    div.appendChild(input);
+    div.appendChild(title);
+    div.appendChild(button);
+    li.appendChild(div);
+    li.appendChild(editInput);
     todoList.appendChild(li);
-
 }
 
 function toggleTodoItem(e) {
-    let todoItem = e.target.parentElement;
+    let todoItem = e.target.parentElement.parentElement;
+    console.log(todoItem);
     if (e.target.checked) {
         todoItem.className = 'completed';
         return
@@ -45,6 +51,31 @@ function toggleTodoItem(e) {
 
 function deleteTodoItem(e) {
     let todoList = document.getElementById('todo-list');
-    let todoItem = e.target.parentElement;
+    let todoItem = e.target.parentElement.parentElement;
     todoList.removeChild(todoItem);
+}
+
+function changeInputMode(e) {
+    let todoItem = e.target.parentElement.parentElement;
+    todoItem.className = 'editing';
+    todoItem.addEventListener('keydown', finishEditMode);
+    todoItem.removeEventListener('dblclick', changeInputMode);
+}
+
+function finishEditMode(e) {
+     if (e.keyCode == 27 ) {
+         let todoItem = e.target.parentElement;
+         todoItem.removeAttribute('class');
+         todoItem.addEventListener('dblclick', changeInputMode);
+         return
+     }
+
+     if (e.keyCode == 13) {
+         let todoItem = e.target.parentElement;
+         const title = todoItem.firstElementChild.getElementsByClassName('label')[0];
+         const editTitle = todoItem.getElementsByClassName('edit')[0];
+         title.innerHTML = editTitle.value;
+         todoItem.removeAttribute('class');
+         todoItem.addEventListener('dblclick', changeInputMode);
+     }
 }
