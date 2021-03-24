827c2642a4fecaef1ef1a64970cf4116f9eea92f
NewWisdom <wlgp2500@gmail.com>
2021-03-24 16:21:41 +0900

    feat(): todo list를 더블클릭했을 때 input 모드로 변경

--- src/App.js
+++ src/App.js
@@ -10,16 +10,32 @@ window.onload = function() {
             const newTodoLI = document.createElement('li');
             newTodoLI.className = "todo";
             newTodoLI.innerHTML = `
-            <div class="view">
+            <div class="view" ondblclick = "editTodo(this)">
                 <input class="toggle" type="checkbox" onclick="check(this)">
                 <label class="label">${inputText}</label>
                 <button class="destroy" onclick= "destroyTodo(this)"></button>
-            </div>`;
+            </div>
+            <input class="edit" value="${inputText}" />`;
+            newTodoLI.addEventListener("keydown", changeMode);
             todoList.appendChild(newTodoLI);
+            
         }
     }
+    
+    
 }
+function changeMode(e){
+    const originalValue = this.getElementsByTagName("label")[0].innerHTML;
+    const editedValue = this.getElementsByTagName("input")[1].value;
 
+    if(e.keyCode == 27) {
+        this.classList.remove("editing");
+    }
+    if(e.keyCode == 13 && (originalValue != editedValue)) {
+        this.getElementsByTagName("label")[0].innerHTML = editedValue;
+        this.classList.remove("editing");
+    }
+}
 function check(box) {
     if (box.checked == true) {
         box.parentNode.parentNode.className = "completed";
@@ -32,4 +48,9 @@ function check(box) {
 function destroyTodo(todo) {
     const todoParent  = todo.parentNode.parentNode;
     todoParent.parentNode.removeChild(todoParent);
+}
+
+function editTodo(editInput) {
+    const editInputParent = editInput.parentNode;
+    editInputParent.classList.add('editing');
 }
