ff8a3d4d364fbdbf37df51dde463d17663603108
joanne <mathmjseo@naver.com>
2021-03-24 20:36:33 +0900

    feat: todoItem을 키보드로 입력하여 추가하는 기능 구현

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,23 @@
-function enter() {
-    var text = document.getElementById("new-todo-title");
+const $todoInput = document.querySelector("#new-todo-title");
 
-    if (window.event.keyCode == 13) {
-        console.log(text.value);
-    }
+$todoInput.addEventListener("keyup", onAddTodoItem);
+
+function onAddTodoItem(event) {
+  const todoTitle = event.target.value;
+  const todoList = document.getElementById("todo-list");
+  if (event.key === "Enter" && todoTitle !== "") {
+    todoList.insertAdjacentHTML("beforeend", renderTodoItemTemplate(todoTitle));
+    event.target.value = "";
+  }
+}
+
+function renderTodoItemTemplate(title) {
+  return ` <li>
+                  <div class="view">
+                      <input class="toggle" type="checkbox">
+                      <label class="label">${title}</label>
+                      <button class="destroy"></button>
+                  </div>
+                  <input class="edit" value="새로운 타이틀">
+              </li>`;
 }
