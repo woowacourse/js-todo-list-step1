c8a30e0f65ebfbb98f1d1d44110f3dd14fcd894d
YeongBeom Heo <hybeom@gmail.com>
2021-03-25 01:50:39 +0900

    feat(src/js/showDoCondition) : 업무별 숨기기 구현

---
+++ src/js/showDoCondition.js
@@ -0,0 +1,40 @@
+window.onload = function(){
+    const filters = document.getElementsByClassName('filters')[0];
+    const allFilters = filters.getElementsByClassName('all')[0];
+    allFilters.addEventListener('click', showAll);
+    const activeFilters = filters.getElementsByClassName('active')[0];
+    activeFilters.addEventListener('click', showActive);
+    const completeFilters = filters.getElementsByClassName('completed')[0];
+    completeFilters.addEventListener('click', showComplete);
+};
+
+const showAll = () => {
+    const allDos = document.getElementById("todo-list").childNodes;
+    Array.prototype.forEach.call(allDos, function(toDo) {
+        toDo.style.display = "block";
+    });
+}
+
+const showActive = () => {
+    const allDos = document.getElementById("todo-list").childNodes;
+    Array.prototype.forEach.call(allDos, function(toDo) {
+        toDo.style.display = "block";
+    });
+    Array.prototype.forEach.call(allDos, function(toDo) {
+        if (toDo.className == "completed"){
+            toDo.style.display = "none";
+        }
+    });
+}
+
+const showComplete = () => {
+    const allDos = document.getElementById("todo-list").childNodes;
+    Array.prototype.forEach.call(allDos, function(toDo) {
+        toDo.style.display = "block";
+    });
+    Array.prototype.forEach.call(allDos, function(toDo) {
+        if (toDo.className != "completed"){
+            toDo.style.display = "none";
+        }
+    });
+}
