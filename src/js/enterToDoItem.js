5905a106c94f0593e0368b34440aa86ebd76a608
YeongBeom Heo <hybeom@gmail.com>
2021-03-24 11:23:41 +0900

    feat : todo List에 todoItem을 키보드로 입력하여 추가
    구현 사항 :
    - 입력 사항들 구성하여 입력

---
+++ src/js/enterToDoItem.js
@@ -0,0 +1,57 @@
+
+function enterToDoItem() {
+    if (window.event.keyCode == 13) {
+        const toDoParent = document.getElementById("todo-list");
+        const toDoContent = document.getElementById("new-todo-title").value;
+        const singleToDo = foamToDoSingle(toDoContent);
+        toDoParent.appendChild(singleToDo);
+    }
+}
+
+function foamToggleInput() {
+    const elementCheckBox = document.createElement("input");
+    elementCheckBox.setAttribute("class", "toggle");
+    elementCheckBox.setAttribute("type", "checkbox");
+
+    return elementCheckBox;
+}
+
+function foamLabel(newTitle) {
+    const elementLabel = document.createElement("label");
+    elementLabel.setAttribute("class", "label");
+    elementLabel.innerHTML = newTitle;
+    return elementLabel;
+}
+
+function foamButton() {
+    const elementButton = document.createElement("button");
+    elementButton.setAttribute("class", "destroy");
+    return elementButton;
+}
+
+function foamViewDiv(newTitle) {
+    const elementViewDiv = document.createElement("div");
+    
+    elementViewDiv.setAttribute("class", "view");
+    elementViewDiv.appendChild(foamToggleInput());
+    elementViewDiv.appendChild(foamLabel(newTitle));
+    elementViewDiv.appendChild(foamButton());
+
+    return elementViewDiv;
+}
+
+function foamInputEdit(newTitle) {
+    const elementInputEdit = document.createElement("input");
+    elementInputEdit.setAttribute("class", "edit");
+    elementInputEdit.setAttribute("value", newTitle);
+
+    return elementInputEdit;
+}
+
+function foamToDoSingle(newTitle) {
+    const elementToDoSingle = document.createElement("li");
+    elementToDoSingle.appendChild(foamViewDiv(newTitle, content));
+    elementToDoSingle.appendChild(foamInputEdit(newTitle, content));
+
+    return elementToDoSingle;
+}
