6aabf0d199c3fd5d909423ebababae3b05fe0419
joanne <mathmjseo@naver.com>
2021-03-25 15:04:53 +0900

    feat: X버튼을 누르면 아이템이 삭제되는 기능 구현

--- src/js/script.js
+++ src/js/script.js
@@ -7,10 +7,13 @@ $toggleInput.addEventListener("click", onToggleTodoItem);
 function onAddTodoItem(event) {
     const todoTitle = event.target.value;
     const todoList = document.getElementById("todo-list");
+    const todoItem = document.createElement("li");
     if (event.key === "Enter" && todoTitle !== "") {
-        todoList.insertAdjacentHTML("beforeend", renderTodoItemTemplate(todoTitle));
+        todoItem.insertAdjacentHTML("beforeend", renderTodoItemTemplate(todoTitle));
+        todoList.appendChild(todoItem);
         event.target.value = "";
         updateTotalCount(); 
+        addRemoveEventOnDestroyBtn(todoItem);
     }
 }
 
@@ -19,18 +22,25 @@ function onToggleTodoItem(event) {
 }  
 
 function renderTodoItemTemplate(title) {
-  return ` <li>
-                  <div class="view">
-                      <input class="toggle" type="checkbox">
-                      <label class="label">${title}</label>
-                      <button class="destroy"></button>
-                  </div>
-                  <input class="edit" value="새로운 타이틀">
-              </li>`;
-}
+    return `<div class="view">
+                <input class="toggle" type="checkbox">
+                <label class="label">${title}</label>
+                <button class="destroy"></button>
+            </div>
+            <input class="edit" value="새로운 타이틀">`;
+  }
 
 function updateTotalCount() {
     const todoList = document.getElementById("todo-list").getElementsByTagName("li");
     const todoCount = document.getElementsByClassName("todo-count").item(0);
     todoCount.innerHTML = "총 <strong>" + todoList.length + "</strong> 개";
+}
+
+function addRemoveEventOnDestroyBtn(todoItem) {
+    const removeBtn = todoItem.getElementsByClassName("destroy").item(0);
+    removeBtn.addEventListener('click', function() {
+        const todoList = document.getElementById("todo-list");
+        todoList.removeChild(todoItem);
+        updateTotalCount();
+    });
 }
