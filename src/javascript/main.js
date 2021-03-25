d0cb7cd45dd002a2cba4b01bd48c74c5d0c44c1a
sakjung <ssangyu123@gmail.com>
2021-03-25 20:55:56 +0900

    refactor: 샘플코드 기반으로 리팩토링

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,78 +1,38 @@
-let id = 1;
-const addInput = document.querySelector('#new-todo-title');
-const toDoList = document.querySelector('#todo-list');
+const $todoInput = document.querySelector("#new-todo-title");
+const $todoList = document.querySelector("#todo-list");
 
-addInput.addEventListener('keyup', (event) => {
-  event.preventDefault();
-  if (!event.isComposing && event.key === 'Enter') {
-    addList();
-  }
-});
+$todoInput.addEventListener("keyup", onAddTodoItem);
+$todoList.addEventListener("click", onToggleTodoItem);
+$todoList.addEventListener('click', onRemoveTodoItem);
 
-toDoList.addEventListener("change", function (e) {
-  if (e.target && e.target.className == "toggle") {
-    checkBox(e.target);
-  }
-});
-
-toDoList.addEventListener('click', function(e) {
-  if (e.target && e.target.className == "destroy") {
-    remove(e.target);
-  }
-});
-
-function checkBox(target) {
-  if (target.checked) {
-   target.parentNode.parentNode.className = 'completed';
-  } else {
-   target.parentNode.parentNode.className = 'false';
+function onAddTodoItem(event) {
+  const todoTitle = event.target.value;
+  const todoList = document.getElementById("todo-list");
+  if (event.key === "Enter" && todoTitle !== "") {
+    todoList.insertAdjacentHTML("beforeend", renderTodoItemTemplate(todoTitle));
+    event.target.value = "";
   }
 }
 
-function remove(target) {
-  const li = target.parentNode.parentNode;
-  li.parentNode.removeChild(li);
+function onToggleTodoItem(event) {
+  event.target.closest("li").classList.toggle("completed");
 }
 
-function addList() {
-  if (addInput.value === '') {
-    alert("입력값이 필요합니다.")
-  } else {
-    const ul = document.querySelector('#todo-list');
-    const li = createLi();
-    addInput.value = '';
-    ul.appendChild(li);
+function renderTodoItemTemplate(title) {
+  return ` <li>
+              <div class="view">
+                  <input class="toggle" type="checkbox">
+                  <label class="label">${title}</label>
+                  <button class="destroy"></button>
+              </div>
+              <input class="edit" value="새로운 타이틀">
+          </li>`;
+}
+
+function onRemoveTodoItem(event) {
+  const target = event.target;
+  if (target && target.className == "destroy") {
+    const li = target.parentNode.parentNode;
+    li.parentNode.removeChild(li);
   }
-}
-
-// create list
-function createLi() {
-  const li = document.createElement('li');
-  li.id = id;
-  li.className = 'false';
-
-  const div = document.createElement('div');
-  div.className = 'view';
-
-  const input = document.createElement('input');
-  input.type = 'checkbox';
-  input.id = id;
-  input.className = 'toggle';
-
-  const label = document.createElement('label');
-  label.className = 'label';
-  label.textContent = addInput.value;
-
-  const button = document.createElement('button');
-  button.className = 'destroy';
-  button.id = 'id';
-
-  div.appendChild(input);
-  div.appendChild(label);
-  div.appendChild(button);
-  li.appendChild(div);
-
-  id = id + 1;
-
-  return li
 }
