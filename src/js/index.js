4bf2a776c202fdddc20c3678b63a32d6d9406129
Joy <kjw11077naver@gmail.com>
2021-03-26 02:01:34 +0900

    feat: todoItem 수정 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,61 @@
 const $todoInput = document.querySelector("#new-todo-title");
-const $toggleInput = document.querySelector("#todo-list");
+const $todoList = document.querySelector("#todo-list");
 
 $todoInput.addEventListener("keyup", onAddTodoItem);
-$toggleInput.addEventListener("click", onToggleTodoItem);
-$toggleInput.addEventListener("click", onDestroyTodoItem);
+$todoList.addEventListener("click", onToggleTodoItem);
+$todoList.addEventListener("click", onDestroyTodoItem);
+$todoList.addEventListener("dblclick", onEditingTodoItem);
+$todoList.addEventListener("keyup", onEditTodoItemComplete);
 
 function onAddTodoItem(event) {
     const todoTitle = event.target.value;
-    const todoList = document.getElementById("todo-list");
     if (event.key === "Enter" && todoTitle !== "") {
-        todoList.insertAdjacentHTML("beforeend", renderTodoItemTemplate(todoTitle));
+        $todoList.insertAdjacentHTML("beforeend", renderTodoItemTemplate(todoTitle));
         event.target.value = "";
     }
 }
 
 function onToggleTodoItem(event) {
-    if (event.target.className == 'toggle'){
+    if (event.target.className == 'toggle') {
         event.target.closest("li").classList.toggle("completed");
     }
     event.stopPropagation();
 }
 
 function onDestroyTodoItem(event) {
-    if (event.target.className == 'destroy'){
-        $toggleInput.removeChild(event.target.closest("li"))
-        // event.target.closest("li").classList.toggle("completed");
+    if (event.target.className == 'destroy') {
+        $todoList.removeChild(event.target.closest("li"))
     }
+    event.stopPropagation();
+}
+
+function onEditingTodoItem(event) {
+    if (event.target.className == 'label') {
+        let beforeText = event.target.innerText
+        event.target.closest("li").classList.toggle("editing");
+        let $edit = event.target.parentElement.parentElement.querySelector('.edit')
+        $edit.value = beforeText
+    }
+    event.stopPropagation();
+}
+
+function onEditTodoItemComplete(event) {
+    if (event.target.className != 'edit') {
+        return;
+    }
+
+    const editedTodoTitle = event.target.value
+    const $edit = event.target.parentElement.parentElement.querySelector('.label')
+    const $li = event.target.closest("li");
+
+    if (event.key === "Enter" && editedTodoTitle !== "") {
+        $li.classList.toggle("editing");
+        $edit.innerText = editedTodoTitle
+
+    } else if (event.key === "Escape") {
+        $li.classList.toggle("editing");
+    }
+
     event.stopPropagation();
 }
 
