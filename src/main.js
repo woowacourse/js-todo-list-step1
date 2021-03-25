b3d7c0b34bfd062f5cc7c03a485f70374e3abfbe
pjy1368 <basejin0810@naver.com>
2021-03-25 20:09:01 +0900

    feat : 상태값에 따라 해야할 일과 완료한 일 보여주는 기능 구현

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 const $todoInput = document.querySelector("#new-todo-title");
 const $todoCount = document.querySelector(".todo-element-count");
+const $toggleInput = document.querySelector(".todo-list");
+const $filter = document.querySelector(".filters");
 
 $todoInput.addEventListener("keyup", onAddTodoItem);
+$filter.addEventListener("click", filterTodoItem);
+
+function filterTodoItem(event) {
+  const classes = $toggleInput.querySelectorAll("li");
+  const buttonName = event.target.className;
+
+  if (buttonName === "completed") {
+    completed(classes);
+    return;
+  }
+  if (buttonName === "active") {
+    onlyTodo(classes);
+    return;
+  }
+  allSelectedTodo(classes);
+}
 
 function onAddTodoItem(event) {
   const todoTitle = event.target.value;
@@ -9,10 +27,9 @@ function onAddTodoItem(event) {
   if (event.key === "Enter" && todoTitle !== "") {
     todoList.insertAdjacentHTML("beforeend", renderTodoItemTemplate(todoTitle));
     event.target.value = "";
-    
     updateCount();
   }
-  
+
 }
 
 function renderTodoItemTemplate(title) {
@@ -50,7 +67,7 @@ function edit(event) {
     item.parentElement.parentElement.classList.add("editing");
 
     const input = document.querySelector(".edit")
-    input.addEventListener('keyup', function(e) {
+    input.addEventListener('keyup', function (e) {
       if (e.key == 'Esc' || e.key == 'Escape') {
         item.parentElement.parentElement.classList.remove("editing");
       }
@@ -66,4 +83,30 @@ function edit(event) {
 function updateCount() {
   const count = document.querySelectorAll('.todo-item').length;
   document.querySelector('.todo-count-text').innerHTML = count;
+}
+
+function allSelectedTodo(todoItems) {
+  todoItems.forEach(element => {
+    element.style.display = "block";
+  })
+}
+
+function onlyTodo(todoItems) {
+  todoItems.forEach(element => {
+    if (element.classList.contains("completed")) {
+      element.style.display = "none";
+    } else {
+      element.style.display = "block";
+    }
+  })
+}
+
+function completed(todoItems) {
+  todoItems.forEach(element => {
+    if (element.classList.contains("completed")) {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  })
 }
