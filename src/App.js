da8513aeb7a4b3aa9da480d7e0e5032674829e89
Sol ji Choi <bssj9307@gmail.com>
2021-03-25 17:40:14 +0900

    style: 코드 리포맷팅

--- src/App.js
+++ src/App.js
@@ -3,48 +3,49 @@ export default class App {
     this.newTodo = document.querySelector('#new-todo-title')
     this.todoList = document.querySelector('#todo-list')
 
-    this.createTodo( this.newTodo, this.todoList )
+    this.createTodo(this.newTodo, this.todoList)
+    this.editTodo(this.todoList)
   }
 
-  createTodo( newTodo, todoList ) {
+  createTodo(newTodo, todoList) {
     newTodo.addEventListener('keyup', (e) => {
-    if (e.key === 'Enter' && newTodo.value) {
-      const todoItem = document.createElement('li')
-      todoItem.className = 'false'
+      if (e.key === 'Enter' && newTodo.value) {
+        const todoItem = document.createElement('li')
+        todoItem.className = 'false'
 
-      const todo = document.createElement('div')
-      todo.className = 'view'
+        const todo = document.createElement('div')
+        todo.className = 'view'
 
-      const checkBox = document.createElement('input')
-      checkBox.type = 'checkbox'
-      checkBox.className = 'toggle'
-      checkBox.addEventListener('click', () => {
-        if (checkBox.checked) {
-          todoItem.className = 'completed'
-        } else {
-          todoItem.className = 'false'
-        }
-      })
+        const checkBox = document.createElement('input')
+        checkBox.type = 'checkbox'
+        checkBox.className = 'toggle'
+        checkBox.addEventListener('click', () => {
+          if (checkBox.checked) {
+            todoItem.className = 'completed'
+          } else {
+            todoItem.className = 'false'
+          }
+        })
 
-      const content = document.createElement('label')
-      content.className = 'label'
-      content.innerText = this.newTodo.value
+        const content = document.createElement('label')
+        content.className = 'label'
+        content.innerText = this.newTodo.value
 
-      const destroyBtn = document.createElement('button')
-      destroyBtn.className = 'destroy'
-      destroyBtn.addEventListener('click', () => {
-        todoList.removeChild(todoItem)
-      })
+        const destroyBtn = document.createElement('button')
+        destroyBtn.className = 'destroy'
+        destroyBtn.addEventListener('click', () => {
+          todoList.removeChild(todoItem)
+        })
 
-      todo.appendChild(checkBox)
-      todo.appendChild(content)
-      todo.appendChild(destroyBtn)
-      todoItem.appendChild(todo)
-      todoList.appendChild(todoItem)
+        todo.appendChild(checkBox)
+        todo.appendChild(content)
+        todo.appendChild(destroyBtn)
+        todoItem.appendChild(todo)
+        todoList.appendChild(todoItem)
 
-      newTodo.value = ''
-    }
-  })
+        newTodo.value = ''
+      }
+    })
   }
 }
 
