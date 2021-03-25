1413206a9830226e05db0e7d499d268085c81ef8
naeunkim <knae11@naver.com>
2021-03-25 17:50:54 +0900

    feat: todo리스트 체크박스 completed 상태로 변경

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,14 @@
 const $todoInput = document.querySelector(".new-todo");
 const $todoUl = document.querySelector(".todo-list");
-console.log($todoInput);
+const date = new Date();
 
 function getTodoInput({key}) {
-    if (key === "Enter") {
-        const todoValue = $todoInput.value;
+    if (key === "Enter" && $todoInput.value !== "") {
         const list = `
-            <li>
+            <li id="${date.getTime()}" class="false">
                 <div class="view">
                     <input class="toggle" type="checkbox"/>
-                    <label class="label">${todoValue}</label>
+                    <label class="label">${$todoInput.value}</label>
                     <button class="destroy"></button>
                 </div>
                 <input class="edit" value="새로운 타이틀" />
@@ -20,3 +19,12 @@ function getTodoInput({key}) {
 }
 
 $todoInput.addEventListener("keyup", getTodoInput);
+
+function modifyList({target}) {
+    if(target.className === 'toggle'){
+        target.closest("li").classList.toggle("completed");
+        target.closest("li").classList.toggle("false");
+    }
+}
+
+$todoUl.addEventListener("click", modifyList)
