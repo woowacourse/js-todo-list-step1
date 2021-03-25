f76d0035ca952eb4112293a7dc005c1411f83737
sakjung <ssangyu123@gmail.com>
2021-03-25 23:53:52 +0900

    feat: edit 기능 추가

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,13 +1,15 @@
 const $todoInput = document.querySelector("#new-todo-title");
 const $todoList = document.querySelector("#todo-list");
 
-$todoInput.addEventListener("keyup", onAddTodoItem);
-$todoList.addEventListener("click", onToggleTodoItem);
+$todoInput.addEventListener('keyup', onAddTodoItem);
+$todoList.addEventListener('click', onToggleTodoItem);
 $todoList.addEventListener('click', onRemoveTodoItem);
+$todoList.addEventListener('dblclick', onChangeToEditMode);
+$todoList.addEventListener('keyup', onFinishedEditMode);
 
 function onAddTodoItem(event) {
   const todoTitle = event.target.value;
-  const todoList = document.getElementById("todo-list");
+  const todoList = document.querySelector("#todo-list");
   if (event.key === "Enter" && todoTitle !== "") {
     todoList.insertAdjacentHTML("beforeend", renderTodoItemTemplate(todoTitle));
     event.target.value = "";
@@ -15,7 +17,10 @@ function onAddTodoItem(event) {
 }
 
 function onToggleTodoItem(event) {
-  event.target.closest("li").classList.toggle("completed");
+  const target = event.target;
+  if (target && target.className === "toggle") {
+    target.closest("li").classList.toggle("completed");
+  }
 }
 
 function renderTodoItemTemplate(title) {
@@ -31,8 +36,39 @@ function renderTodoItemTemplate(title) {
 
 function onRemoveTodoItem(event) {
   const target = event.target;
-  if (target && target.className == "destroy") {
+  if (target && target.className === "destroy") {
     const li = target.parentNode.parentNode;
     li.parentNode.removeChild(li);
   }
+}
+
+function onChangeToEditMode(event) {
+    event.preventDefault();
+    const target = event.target;
+    const todoItem = target.closest('li');
+    if (todoItem.className !== 'editing') {
+        todoItem.classList.toggle('editing');
+    }
+}
+
+function onFinishedEditMode(event) {
+  event.preventDefault();
+  const target = event.target;
+
+  if (target && target.className === 'edit' && event.key === 'Escape') {
+      document.getSelection().anchorNode.classList.remove('editing');
+  } else if (target && target.value !== "" &&
+  target.className === 'edit' && event.key === 'Enter') {
+    onUpdate(target);
+    document.getSelection().anchorNode.classList.remove('editing');
+  }
+}
+
+function onUpdate(target) {
+  const div = target.closest('li').querySelector('div');
+  const originalLabel = div.querySelector('label');
+  const newLabel = document.createElement('label');
+  newLabel.className = 'label';
+  newLabel.textContent = target.value;
+  div.replaceChild(newLabel, originalLabel);
 }
