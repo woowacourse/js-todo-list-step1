985f536a565a48f7b74c81b6c05f291f924eabc9
joanne <mathmjseo@naver.com>
2021-03-25 13:42:38 +0900

    feat: todoList의 아이템 개수를 리스트 하단에 보여주는 기능 추가

--- src/js/script.js
+++ src/js/script.js
@@ -5,17 +5,18 @@ $todoInput.addEventListener("keyup", onAddTodoItem);
 $toggleInput.addEventListener("click", onToggleTodoItem);
 
 function onAddTodoItem(event) {
-  const todoTitle = event.target.value;
-  const todoList = document.getElementById("todo-list");
-  if (event.key === "Enter" && todoTitle !== "") {
-    todoList.insertAdjacentHTML("beforeend", renderTodoItemTemplate(todoTitle));
-    event.target.value = "";
-  }
+    const todoTitle = event.target.value;
+    const todoList = document.getElementById("todo-list");
+    if (event.key === "Enter" && todoTitle !== "") {
+        todoList.insertAdjacentHTML("beforeend", renderTodoItemTemplate(todoTitle));
+        event.target.value = "";
+        updateTotalCount(); 
+    }
 }
 
 function onToggleTodoItem(event) {
     event.target.closest("li").classList.toggle("completed");
-  }  
+}  
 
 function renderTodoItemTemplate(title) {
   return ` <li>
@@ -26,4 +27,10 @@ function renderTodoItemTemplate(title) {
                   </div>
                   <input class="edit" value="새로운 타이틀">
               </li>`;
+}
+
+function updateTotalCount() {
+    const todoList = document.getElementById("todo-list").getElementsByTagName("li");
+    const todoCount = document.getElementsByClassName("todo-count").item(0);
+    todoCount.innerHTML = "총 <strong>" + todoList.length + "</strong> 개";
 }
