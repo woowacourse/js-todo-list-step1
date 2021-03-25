aacbb99a111eccf4cf6f72e0230a34c27370a66c
kimjunseo <air.junseo@gmail.com>
2021-03-25 17:04:24 +0900

    feat : 더블클릭했을 때 input 모드로 변경

--- src/js/todoItem.js
+++ src/js/todoItem.js
@@ -18,17 +18,37 @@ function todoItemTemplate(item) {
                     <label class="label">${item}</label>
                     <button class="destroy"></button>
                 </div>
-                <input class="edit" value="새로운 타이틀">
+                <input class="edit" value="${item}" placeholder="새로운 타이틀">
             </li>`;
 }
 
 document.getElementById("todo-list")
     .addEventListener("click", function (e) {
-    if (e.target.classList.contains("toggle")) {
-        e.target.closest("li").classList.toggle("completed");
-        // 체크박스에 checked 추가
+        if (e.target.classList.contains("toggle")) {
+            e.target.closest("li").classList.toggle("completed");
+            // 체크박스에 checked 추가
+        }
+        if (e.target.classList.contains("destroy")) {
+            e.target.closest("li").remove();
+        }
+    });
+
+document.getElementById("todo-list")
+    .addEventListener("dblclick", function (e) {
+        e.target.closest("li").classList.toggle("editing");
+        const editInput = e.target.closest("li").getElementsByClassName("edit")[0];
+        editInput.addEventListener("keyup", edit);
+    });
+
+function edit(event) {
+    const item = event.target.value;
+    if (event.key === "Enter" && item !== "") {
+        const parent = event.target.parentNode;
+        const label = parent.getElementsByClassName("label")[0];
+        label.innerText = item;
+        parent.classList.toggle("editing");
     }
-    if (e.target.classList.contains("destroy")) {
-        e.target.closest("li").remove();
+    if (event.key === "Escape") {
+        event.target.parentNode.classList.toggle("editing");
     }
-});
+}
