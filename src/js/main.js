6b34f3995cd3c25207cb9d323cc4352a03b6ce4f
naeunkim <knae11@naver.com>
2021-03-26 08:48:07 +0900

    refactor: id 중복 문제 해결

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,18 @@
 const $todoInput = document.querySelector(".new-todo");
 const $todoUl = document.querySelector(".todo-list");
-const date = new Date();
+const $todoCount = document.querySelector(".todo-count");
+const $all = document.querySelector(".all");
+const $active = document.querySelector(".active");
+const $completed = document.querySelector(".completed");
 
-function addTodoInput({key}) {
+
+let todos = {}
+
+const updateCount = () => $todoCount.querySelector("strong").innerText = $todoUl.children.length
+
+const addTodoInput = ({key}) => {
     if (key === "Enter" && $todoInput.value !== "") {
-        const id = date.getTime();
+        const id = new Date().getTime();
         const value = $todoInput.value;
         const list = `
             <li id="${id}" class="false">
@@ -16,13 +24,19 @@ function addTodoInput({key}) {
                 <input class="edit" value="${value}" />
             </li>`;
         $todoUl.insertAdjacentHTML('beforeend', list);
+        const temporary = {...todos}
+        temporary[id] = {
+            'completed': false,
+            'contents': value
+        }
+        todos = {... temporary}
+        console.log(todos)
         $todoInput.value = '';
+        updateCount()
     }
 }
 
-$todoInput.addEventListener("keyup", addTodoInput);
-
-function modifyTodoState({target}) {
+const modifyTodoState = ({target}) => {
     if (target.className === 'toggle') {
         target.closest("li").classList.toggle("completed");
         target.closest("li").classList.toggle("false");
@@ -32,29 +46,32 @@ function modifyTodoState({target}) {
 
     if (target.className === 'destroy') {
         $todoUl.removeChild(target.closest("li"))
+        updateCount()
     }
 }
 
-$todoUl.addEventListener("click", modifyTodoState)
 
-function editContent(event) {
+const editContent = (event) => {
     const {key} = event
     const {currentTarget} = event
     const previousInput = currentTarget.querySelector(".label");
     if (key === "Enter") {
-        previousInput.innerText =  event.target.value;
+        previousInput.innerText = event.target.value;
         currentTarget.classList.toggle("editing")
     }
     if (key === "Escape") {
         currentTarget.classList.toggle("editing")
     }
-
 }
 
-function editingMode({target}) {
+const editingMode = ({target}) => {
     const $li = target.closest("li");
     $li.classList.toggle("editing");
     $li.addEventListener("keyup", editContent);
 }
 
+$todoUl.addEventListener("click", modifyTodoState)
+$todoInput.addEventListener("keyup", addTodoInput)
 $todoUl.addEventListener("dblclick", editingMode)
+$all.addEventListener("click", () => console.log("all"))
+
