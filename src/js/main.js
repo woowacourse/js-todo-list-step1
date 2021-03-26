6b11e7a8114265a55ca343b138a586e11b7b25ab
naeunkim <knae11@naver.com>
2021-03-26 18:04:16 +0900

    feat: 해야할일, 완료된일에 따라 리스트 보이는 기능구현 완료

--- src/js/main.js
+++ src/js/main.js
@@ -5,17 +5,17 @@ const $all = document.querySelector(".all");
 const $active = document.querySelector(".active");
 const $completed = document.querySelector(".completed");
 
+const CONTENT = 'content'
+const COMPLETED = 'completed'
+const ID = 'id'
+const FALSE = 'false'
+const CHECKED = 'checked'
 
 let todos = {}
 
-const updateCount = () => $todoCount.querySelector("strong").innerText = $todoUl.children.length
-
-const addTodoInput = ({key}) => {
-    if (key === "Enter" && $todoInput.value !== "") {
-        const id = new Date().getTime();
-        const value = $todoInput.value;
-        const list = `
-            <li id="${id}" class="false">
+const updateCount = (count) => $todoCount.querySelector("strong").innerText = count
+const list = (id, completed, value) => `
+            <li id="${id}" class=${completed ? COMPLETED : FALSE }>
                 <div class="view">
                     <input class="toggle" type="checkbox" id="${id}"/>
                     <label class="label">${value}</label>
@@ -23,30 +23,57 @@ const addTodoInput = ({key}) => {
                 </div>
                 <input class="edit" value="${value}" />
             </li>`;
-        $todoUl.insertAdjacentHTML('beforeend', list);
+
+
+const addTodoInput = ({key}) => {
+    if (key === "Enter" && $todoInput.value !== "") {
+        const id = Date.now();
+        const content = $todoInput.value;
+
+        $todoUl.insertAdjacentHTML('beforeend', list(id, false, content))
         const temporary = {...todos}
         temporary[id] = {
             'completed': false,
-            'contents': value
+            'content':  content
         }
         todos = {...temporary}
-        console.log(todos)
         $todoInput.value = '';
-        updateCount()
+
+        updateCount(Object.keys(todos).length)
     }
 }
 
 const modifyTodoState = ({target}) => {
     if (target.className === 'toggle') {
-        target.closest("li").classList.toggle("completed");
-        target.closest("li").classList.toggle("false");
+        const item = target.closest("li").classList
+        const targetId = target.closest("li").getAttribute("id");
+        if (item.contains('false')) {
+            item.toggle('false');
+            item.toggle('completed');
+            temporary = {...todos}
+            temporary[targetId]['completed'] = true;
+            todos = {...temporary}
+            return;
+        }
+        if (item.contains('completed')) {
+            item.toggle('false');
+            item.toggle('completed');
+            temporary = {...todos}
+            temporary[targetId]['completed'] = false;
+            todos = {...temporary}
+            return;
+        }
 
-        target.toggleAttribute("checked")
+        target.toggleAttribute(CHECKED)
     }
 
     if (target.className === 'destroy') {
+        const targetId = target.closest("li").getAttribute("id");
         $todoUl.removeChild(target.closest("li"))
-        updateCount()
+        temporary = {...todos}
+        delete temporary[targetId]
+        todos = {...temporary}
+        updateCount(Object.keys(todos).length)
     }
 }
 
@@ -55,9 +82,15 @@ const editContent = (event) => {
     const {key} = event
     const {currentTarget} = event
     const previousInput = currentTarget.querySelector(".label");
+    const targetId = event.currentTarget.getAttribute("id");
     if (key === "Enter") {
-        previousInput.innerText = event.target.value;
+        const currentContent = event.target.value;
+        previousInput.innerText = currentContent;
         currentTarget.classList.toggle("editing")
+        temporary = {...todos}
+        temporary[targetId]['content'] = currentContent;
+        todos = {...temporary}
+        console.log(todos)
     }
     if (key === "Escape") {
         currentTarget.classList.toggle("editing")
@@ -73,5 +106,42 @@ const editingMode = ({target}) => {
 $todoUl.addEventListener("click", modifyTodoState)
 $todoInput.addEventListener("keyup", addTodoInput)
 $todoUl.addEventListener("dblclick", editingMode)
-$all.addEventListener("click", () => console.log("all"))
+
+function showAll() {
+    const allTodos = Object.keys(todos)
+        .map(id => list(id, todos[id]['completed'], todos[id]['content']))
+        .join('')
+    $todoUl.innerHTML = allTodos;
+    updateCount(Object.keys(todos).length)
+}
+
+$all.addEventListener("click", showAll)
+
+function showActive() {
+
+    const activeList = Object.keys(todos)
+        .filter(id => todos[id]['completed'] === false)
+        .map(id => list(id, todos[id]['completed'], todos[id]['content']))
+        .join("");
+
+    $todoUl.innerHTML = activeList;
+    updateCount(Object.keys(todos).filter(id => todos[id]['completed'] === false).length)
+
+}
+
+$active.addEventListener("click", showActive)
+
+function showCompleted() {
+
+    const completedTodos = Object.keys(todos)
+        .filter(id => todos[id]['completed'] === true)
+        .map(id => list(id, todos[id]['completed'], todos[id]['content']))
+        .join("");
+
+    $todoUl.innerHTML = completedTodos;
+    updateCount(Object.keys(todos).filter(id => todos[id]['completed'] === true).length)
+
+}
+
+$completed.addEventListener("click", showCompleted)
 
