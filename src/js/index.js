467f39c1b394a5d4f2386ac85904a54e85e0a906
TaewanKimmmm <ktww88@gmail.com>
2021-03-27 18:04:33 +0900

    feat: 모든 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,67 @@
-const $input = document.getElementById("new-todo-title");
-$input.addEventListener('keyup', addTodoItem);
+const $todoInput = document.getElementById("new-todo-title");
+$todoInput.addEventListener('keyup', addTodoItem);
 
-const $ul = document.getElementById("todo-list");
+const $todoList = document.getElementById("todo-list");
+$todoList.addEventListener('click', completeOrDelete)
+$todoList.addEventListener('dblclick', editTodoItem)
+$todoList.addEventListener('keyup', cancelEditItem)
 
-$ul.addEventListener('click', makeItemCompleted)
-$ul.addEventListener('click', deleteItem)
+const $allSelected = document.querySelector(".selected");
+const $active = document.querySelector(".active");
+const $completed = document.querySelector(".completed");
 
+function showAll(event) {
+    $active.classList.remove("selected")
+    $completed.classList.remove("selected")
+
+    event.target.classList.toggle("selected")
+    Array.from($todoList.children).forEach(element => element.style.display = "block")
+    updateTodoItemCount($todoList.childElementCount);
+}
+
+function showTodo(event) {
+    $allSelected.classList.remove("selected")
+    $completed.classList.remove("selected")
+
+    event.target.classList.toggle("selected")
+    const completed = Array.from($todoList.children).filter(element => element.classList.contains("completed"));
+    const notCompleted = Array.from($todoList.children).filter(element => !element.classList.contains("completed"));
+
+    notCompleted.forEach(element => element.style.display = 'block')
+    completed.forEach(element => element.style.display = 'none')
+    updateTodoItemCount(notCompleted.length)
+}
+
+function showCompleted(event) {
+    $allSelected.classList.remove("selected")
+    $active.classList.remove("selected")
+
+    event.target.classList.toggle("selected")
+    const completed = Array.from($todoList.children).filter(element => element.classList.contains("completed"));
+    const notCompleted = Array.from($todoList.children).filter(element => !element.classList.contains("completed"));
+
+    completed.forEach(element => element.style.display = 'block')
+    notCompleted.forEach(element => element.style.display = 'none')
+    updateTodoItemCount(completed.length)
+}
+
+$allSelected.addEventListener('click', showAll)
+$active.addEventListener('click', showTodo)
+$completed.addEventListener('click', showCompleted)
+
+const $count = document.querySelector("strong")
 
 function addTodoItem(event) {
     if (event.key === 'Enter' && event.target.value !== "") {
-        const addedItem = getTodoItem($input.value);
-        $ul.insertAdjacentHTML("beforeend", addedItem);
-        $input.value = null;
+        const addedItem = getTodoItem($todoInput.value);
+        $todoList.insertAdjacentHTML("beforeend", addedItem);
+        $todoInput.value = null;
     }
+    updateTodoItemCount($todoList.childElementCount);
 }
 
 function getTodoItem(itemTitle) {
-    return` <li>
+    return ` <li>
     <div class="view">
       <input class="toggle" type="checkbox"/>
       <label class="label">${itemTitle}</label>
@@ -26,15 +71,47 @@ function getTodoItem(itemTitle) {
   </li>`
 }
 
-function makeItemCompleted(event) {
-    if (event.target.tagName === 'INPUT') {
-        event.target.closest("li").classList.toggle("completed");
+function completeOrDelete(event) {
+    const todoItem = event.target.closest("li")
+    if (event.target.type === 'checkbox' && !event.target.classList.contains("checked")) {
+        todoItem.classList.toggle("completed");
+        todoItem.querySelector(".toggle").classList.add("checked")
+        return;
+    }
+
+    if (event.target.type === 'checkbox' && event.target.classList.contains("checked")) {
+        todoItem.classList.toggle("completed");
+        todoItem.querySelector(".toggle").classList.remove("checked")
+        return;
+    }
+
+    if (event.target.tagName === 'BUTTON') {
+        todoItem.remove();
+        updateTodoItemCount();
     }
 }
 
-function deleteItem(event) {
-    if (event.target.tagName === 'BUTTON') {
-        event.target.closest("li").remove();
+function updateTodoItemCount(count) {
+    $count.innerText = count;
+}
+
+function editTodoItem(event) {
+    const todoItem = event.target.closest("li");
+    todoItem.classList.add("editing");
+    todoItem.querySelector(".edit").addEventListener('keyup', completeEdit)
+}
+
+function completeEdit(event) {
+    const todoItem = event.target.closest("li");
+    if (event.key === 'Enter') {
+        todoItem.querySelector(".label").textContent = event.target.value;
+        todoItem.classList.remove("editing");
+    }
+}
+
+function cancelEditItem(event) {
+    if (event.target.className === 'edit' && event.key === 'Escape') {
+        event.target.closest("li").classList.remove("editing");
     }
 }
 
