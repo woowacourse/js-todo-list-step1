9bfc77091ab2683ce68f65db030bc8695e874785
rok93 <goodboy302@naver.com>
2021-03-28 22:52:40 +0900

    feat: list의 체크박스를 클릭하여 complete 상태로 변경하는 기능

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,41 +1,46 @@
-function TodoList({$target, todoInput}) {
+function TodoList({$target}) {
 
     this.init = () => {
         this.$target = $target;
         this.render();
+        this.addEvents();
     }
 
     this.render = () => {
-        this.$target.innerHTML = '<ul id="todo-list" class="todo-list">' +
-            '            <li>' +
-            '                <div class="view">' +
-            '                    <input class="toggle" type="checkbox"/>' +
-            '                    <label class="label">새로운 타이틀</label>' +
-            '                    <button class="destroy"></button>' +
-            '                </div>' +
-            '                <input class="edit" value="새로운 타이틀" />' +
-            '            </li>' +
-            '            <li class="editing">' +
-            '                <div class="view">' +
-            '                    <input class="toggle" type="checkbox" />' +
-            '                    <label class="label">완료된 타이틀</label>' +
-            '                    <button class="destroy"></button>' +
-            '                </div>' +
-            '                <input class="edit" value="완료된 타이틀" />' +
-            '            </li>' +
-            '            <li class="completed">' +
-            '                <div class="view">' +
-            '                    <input class="toggle" type="checkbox" checked/>' +
-            '                    <label class="label">완료된 타이틀</label>' +
-            '                    <button class="destroy"></button>' +
-            '                </div>' +
-            '                <input class="edit" value="완료된 타이틀" />' +
-            '            </li>' +
-            '        </ul>';
+
+    }
+
+    this.addEvents = () => {
+        this.$target.addEventListener("click", this.addChangeTodoState);
+    }
+
+    this.addChangeTodoState = (evt) => {
+        const clickedClassName = evt.target.className;
+        if (clickedClassName !== 'destroy' && clickedClassName !== 'toggle') {
+            return;
+        }
+
+        if (clickedClassName === 'toggle') {
+            this.toggleTodoItem(evt);
+        }
+
+        // todo: 수정기능! (추가할 경우 위의 기능은 early return 시키기!)
+
+        // todo: 삭제기능!
+    }
+
+    this.toggleTodoItem = evt => {
+        const clickedTodoState = evt.target.offsetParent.classList;
+        if (evt.target.checked === true) {
+            clickedTodoState.add("completed");
+            return;
+        }
+
+        clickedTodoState.remove("completed");
     }
 
     this.addNewTodoItem = (newTitle) => {
-        $target.innerHTML += `<li>
+        this.$target.innerHTML += `<li>
                             <div id="${newTitle.id}" class="view">
                                 <input class="toggle" type="checkbox"/>
                                 <label class="label">${newTitle.title}</label>
