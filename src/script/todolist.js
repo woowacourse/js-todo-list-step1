d6a1db6d38fa6328b7a4a9eab2a4eadff29afa29
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-03-28 16:50:26 +0900

    feat: 체크박스를 클릭할 경우 완료상태로 변경

--- src/script/todolist.js
+++ src/script/todolist.js
@@ -3,6 +3,8 @@ const $todoList = document.getElementById("todo-list");
 
 $todoInput.addEventListener('keyup', addTodoItem);
 
+$todoList.addEventListener('click', todoComplete)
+
 function addTodoItem(event) {
     if (event.key === 'Enter' && event.target.value !== "") {
         const addedItem = getTodoItem($todoInput);
@@ -11,9 +13,27 @@ function addTodoItem(event) {
     }
 }
 
+function todoComplete(event) {
+    const todoItem = event.target.closest("li")
+
+    if (event.target.type === 'checkbox' &&
+        !event.target.classList.contains("checked")) {
+        todoItem.classList.toggle("completed");
+        todoItem.querySelector(".toggle").classList.add("checked")
+        return;
+    }
+
+    if (event.target.type === 'checkbox' &&
+        event.target.classList.contains("checked")) {
+        todoItem.classList.toggle("completed");
+        todoItem.querySelector(".toggle").classList.remove("checked")
+        return;
+    }
+}
+
 function getTodoItem(itemTitle) {
     return ` <li>
-      
+      <input class="toggle" type="checkbox"/>
       <label class="label">${itemTitle.value}</label>
   </li>`
 }
