f883e035135ffae278d231171d5bcc4df58a1b5a
thisisyoungbin <thisisyoungbin@gmail.com>
2021-03-29 15:02:57 +0900

    feat: list 상태 값을 확인해 상태별 아이템만 보여주는 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const $todoInput = document.querySelector("#new-todo-title");
 const $todoList = document.querySelector(".todo-list");
 
-
 $todoInput.addEventListener("keyup", onAddTodoItem);
 $todoList.addEventListener("click", onDeleteTodoItem);
 $todoList.addEventListener("click", onToggleTodoItem);
 $todoList.addEventListener('dblclick', onEditTodoItem);
+
 
 function onAddTodoItem(event) {
     const todoTitle = event.target.value;
@@ -14,11 +14,12 @@ function onAddTodoItem(event) {
         todoList.insertAdjacentHTML("beforeend", renderTodoItemTemplate(todoTitle));
         event.target.value = "";
     }
-    onTodoCount();
+    var count = $todoList.childElementCount
+    onTodoCount(count);
 }
 
 function renderTodoItemTemplate(title) {
-    return `<li class="all">
+    return `<li>
                 <div class="view">
                     <input class="toggle" type="checkbox"/>
                     <label class="label">${title}</label>
@@ -32,12 +33,14 @@ function onDeleteTodoItem(event) {
     if (event.target.className === "destroy") {
         event.target.closest('li').remove();
     }
-    onTodoCount();
+    var count = $todoList.childElementCount
+    onTodoCount(count);
 }
 
 function onToggleTodoItem(event) {
     if (event.target.className === "toggle") {
         event.target.closest("li").classList.toggle("completed");
+        event.target.toggleAttribute(`checked`);
     }
 }
 
@@ -58,10 +61,73 @@ function onEditTodoItem(event) {
             }
         })
     }
-    onTodoCount();
 }
 
-function onTodoCount() {
+function onTodoCount(value) {
     const strongTag = document.getElementsByClassName("count")[0];
-    strongTag.innerHTML = $todoList.childElementCount;
+    strongTag.innerHTML  = value;
+}
+
+const $filtered = document.querySelector(".filters");
+
+const $allTodo = document.querySelector(".all");
+const $activeTodo = $filtered.querySelector(".active");
+const $completedTodo = $filtered.querySelector(".completed");
+
+$allTodo.addEventListener("click", onFilterAllTodo);
+$activeTodo.addEventListener("click", onFilterActiveTodo);
+$completedTodo.addEventListener("click", onFilterCompletedTodo);
+
+function resetFilter() {
+    document.querySelector(".all").classList.remove("selected");
+    $filtered.querySelector(".active").classList.remove("selected");
+    $filtered.querySelector(".completed").classList.remove("selected");
+}
+
+function onFilterAllTodo() {
+    resetFilter();
+    document.querySelector(".all").classList.add("selected");
+
+    for (const todoItem of $todoList.children) {
+        todoItem.style.display = "block";
+    }
+
+    var count = $todoList.childElementCount
+    onTodoCount(count);
+}
+
+function onFilterActiveTodo() {
+    resetFilter();
+    $filtered.querySelector(".active").classList.add("selected");
+
+    var count = 0;
+
+    for (const todoItem of $todoList.children) {
+        if (!todoItem.classList.contains("completed")) {
+            todoItem.style.display = "block";
+            count += 1;
+        } else {
+            todoItem.style.display = "none";
+        }
+    }
+
+    onTodoCount(count);
+}
+
+function onFilterCompletedTodo() {
+    resetFilter()
+    $filtered.querySelector(".completed").classList.add("selected");
+
+    var count = 0;
+
+    for (const todoItem of $todoList.children) {
+        if (todoItem.classList.contains("completed")) {
+            todoItem.style.display = "block";
+            count += 1;
+        } else {
+            todoItem.style.display = "none";
+        }
+    }
+
+    onTodoCount(count);
 }
